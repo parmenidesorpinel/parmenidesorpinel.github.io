2129ba8942c11c90d3338c0cccee48e935feb194
AlvaroMG07 <57816113+AlvaroMG07@users.noreply.github.com>
2021-03-18 14:16:52 -0700

    add index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 920,
+            settings: {
+                slidesToShow: 4
+            }
+        },{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        } 
+    
+    ]
+    });
+});
