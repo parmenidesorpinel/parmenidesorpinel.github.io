81be94b680235b57b656d16fff9c789ef667a103
sayulvalenz <sayulvalenz@gmail.com>
2021-12-20 23:05:01 -0700

    se centraron los componentes del contador

--- js/countdown.js
+++ js/countdown.js
@@ -12,10 +12,10 @@ simplyCountdown('#countdown', {
         seconds: { singular: ' second', plural: ' seconds' }
     },
     plural: true, //use plurals
-    inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+    inline: true, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
     inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
     // in case of inline set to false
-    enableUtc: false, //Use UTC or not - default : false
+    enableUtc: true, //Use UTC or not - default : false
     onEnd: function() { return; }, //Callback on countdown end, put your own function here
     refresh: 1000, // default refresh every 1s
     sectionClass: 'simply-section', //section css class
