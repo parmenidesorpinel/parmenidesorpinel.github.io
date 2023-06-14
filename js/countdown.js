be67de9ebc85daed9badd3f630db7461fbadf935
IanOlivarez <wannacross@gmail.com>
2023-06-13 17:45:43 -0700

    Corrected for race start times 08/05/23 and 08/06/23

--- js/countdown.js
+++ js/countdown.js
@@ -29,7 +29,7 @@ simplyCountdown('#countdown2', {
     year: 2023, // required
     month: 8, // required
     day: 05, // required
-    hours: 15, // Default is 0 [0-23] integer
+    hours: 08, // Default is 0 [0-23] integer
     minutes: 0, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
@@ -56,7 +56,7 @@ simplyCountdown('#countdown3', {
     year: 2023, // required
     month: 8, // required
     day: 06, // required
-    hours: 15, // Default is 0 [0-23] integer
+    hours: 08, // Default is 0 [0-23] integer
     minutes: 0, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
     words: { //words displayed into the countdown
