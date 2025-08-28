1219f61d656553d5950f98baa144fb87edc55b39
IanOlivarez <wannacross@gmail.com>
2025-08-27 21:15:34 -0600

    2025 Redesign

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         slidesToShow: 7,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1500,
+        autoplaySpeed: 1700,
         arrows: false,
         dots: false,
         pauseOnHover: false,
